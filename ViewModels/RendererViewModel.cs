62959ef3d06af0fab594a95e7722ff4a39593816
shepshook <nikitashepshuk@gmail.com>
2020-02-01 13:24:39 +0300

    Добавьте файлы проекта.

---
+++ ViewModels/RendererViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace rtxon.ViewModels
+{
+    class RendererViewModel
+    {
+        private WriteableBitmap Bitmap;
+
+        public RendererViewModel()
+        {
+            Bitmap = new WriteableBitmap(
+                500,
+                500,
+                96,
+                96,
+                PixelFormats.Rgb24,
+                null);
+        }
+
+        public Image GetImage()
+        {
+            Image Image = new Image();
+
+            for (int i = 0; i < Bitmap.Height; i++)
+                for (int j = 0; j < Bitmap.Width; j++)
+
+        }
+    }
+}
